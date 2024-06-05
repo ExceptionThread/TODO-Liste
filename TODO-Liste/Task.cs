65aa43a0c0a38ec306b061a9e3b212e5084035a7
Riccardo <ricci-2002@gmx.de>
2024-06-05 12:55:07 +0200

    Projektdateien hinzufügen.

---
+++ TODO-Liste/Task.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_ToDoListe
+{
+    internal class Task
+    {
+        /// <summary>
+        /// ID of the task
+        /// </summary>
+        public int ID { get; private set; }
+
+        /// <summary>
+        /// Short description of the task
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Detailed description of the task
+        /// </summary>
+        public string Details { get; set; }
+
+        /// <summary>
+        /// Due Date 
+        /// </summary>
+        public DateTime DueDate { get; set; }
+
+        /// <summary>
+        /// ID_SERVER for new Tasks
+        /// </summary>
+        private static int ID_SERVER = 1000;
+
+        /// <summary>
+        /// Constructor for a new task (without ID)
+        /// </summary>
+        /// <param name="description"></param>
+        /// <param name="details"></param>
+        /// <param name="dueDate"></param>
+        public Task(string description, string details, DateTime dueDate)
+        {
+            Description = description;
+            Details = details;
+            DueDate = dueDate;
+            ID = ID_SERVER;
+            ID_SERVER += 10;
+        }
+
+
+        /// <summary>
+        /// 
+        /// Constructor für a given Task with id
+        /// 
+        /// </summary>
+        /// <param name="iD"></param>
+        /// <param name="description"></param>
+        /// <param name="details"></param>
+        /// <param name="dueDate"></param>
+        public Task(int iD, string description, string details, DateTime dueDate)
+        {
+            ID = iD;
+            Description = description;
+            Details = details;
+            DueDate = dueDate;
+        }
+
+        /// <summary>
+        /// String representation of the task 
+        /// - used for display of list items 
+        /// </summary>
+        /// <returns>string representation of the task</returns>
+        public override string ToString()
+        {
+            return $"TaskID {ID}: {Description} (due: {DueDate.ToShortDateString()})";
+        }
+    }
+}
---
+++ TODO-Liste/ToDos.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_ToDoListe
+{
+    internal class ToDos
+    {
+        /// <summary>
+        /// The task list to be managed
+        /// </summary>
+        private List<Task> taskliste;
+        public List<Task> Taskliste
+        {
+            get { return taskliste; }
+        }
+
+        /// <summary>
+        /// Container for Task
+        /// Manages the Tasklist
+        /// </summary>
+        public ToDos()
+        {
+            taskliste = new List<Task>();
+            AddTestData();
+        }
+
+        /// <summary>
+        /// Creates a new Task and adds it to the tasklist
+        /// </summary>
+        /// <param name="description"></param>
+        /// <param name="details"></param>
+        /// <param name="dueDate"></param>
+        public void AddTask(string description, string details, DateTime dueDate)
+        {
+            if (description == null)
+            {
+                new ArgumentNullException("No Task without description can be added to the todos.");
+            }
+            Task newTask = new Task(description, details, dueDate);
+
+            taskliste.Add(newTask);
+        }
+
+        /// <summary>
+        /// Adds test data to the tasklist 
+        /// 
+        /// </summary>
+        private void AddTestData()
+        {
+            taskliste.Add(new Task("Aufräumen", "Arbeitszimmer entrümpeln und umräumen", DateTime.Today.AddDays(5)));
+            taskliste.Add(new Task("AGC-Teamsaufgaben bearbeiten", "Übung 10 überarbeiten und abgeben", DateTime.Today.AddDays(4)));
+            taskliste.Add(new Task("Datensicherung", "Daten von altem Computer sichern", DateTime.Today));
+            taskliste.Add(new Task("Einkaufen", "Vorräte auffüllen", DateTime.Today));
+        }
+    }
+}
